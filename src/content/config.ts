9584d9a338c3f176ae7c2ea1b7cc0dcc4f62343b
ramirols <ramirols.dev@gmail.com>
2025-12-16 02:35:24 -0500

    :tada: fix de despliegue en vistas

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,18 @@ const blogCollection = defineCollection({
     }),
 });
 
+const portafolioCollection = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        date: z.string().transform((val) => new Date(val)),
+        author: z.string().default("Ramiro Leonardo"),
+        cover: z.string().optional(),
+        category: z.string().optional(),
+    }),
+});
+
 export const collections = {
     blog: blogCollection,
+    portafolio: portafolioCollection,
 };
